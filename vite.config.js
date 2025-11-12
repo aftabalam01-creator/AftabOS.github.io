cdcf7759fd460aefd53c5e7025411cb99d03bd41
Aftab Alam Masjidi <61548017+aftabalam01-creator@users.noreply.github.com>
2025-11-11 21:19:22 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AftabOS.github.io/',
   plugins: [react()],
-})
+  base: '/', // ✅ This tells GitHub Pages to use the root domain
+});
